01281a873f121dc06f05389609288afc0715bc37
Steve Salazar <eugenio.salazar@live.u-tad.com>
2026-01-04 14:20:46 +0100

    Steve - quitando clases

--- src/main/java/com/utad/mck/ProyectoMentiroso/ProyectoMentirosoApplication.java
+++ src/main/java/com/utad/mck/ProyectoMentiroso/ProyectoMentirosoApplication.java
@@ -1,12 +1,9 @@
 package com.utad.mck.ProyectoMentiroso;
 
 import java.util.ArrayList;
-
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
